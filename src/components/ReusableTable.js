27ed14b9e1293fa402959301c3a21e20b4d9738a
naim5829 <md.naimur.rahman.naim@gmail.com>
2023-12-19 23:53:25 +0600

    Added Skeleton Loader on table

--- src/components/ReusableTable.js
+++ src/components/ReusableTable.js
@@ -1,36 +1,64 @@
-import React from 'react'
-import { cascadeDate, replaceNumbers, replaceTimeString } from './showDateInBangla'
-import { showDayInBangla } from './showDayInBangla'
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import {
+  cascadeDate,
+  replaceNumbers,
+  replaceTimeString,
+} from "./showDateInBangla";
+import { showDayInBangla } from "./showDayInBangla";
 
-const ReusableTable = ({ramadanData, firstNumber, lastNumber}) => {
+const ReusableTable = ({ ramadanData, firstNumber, lastNumber }) => {
   return (
     <table>
-    <thead>
-      <tr>
-        <th>রোজা</th>
-        <th>তারিখ</th>
-        <th>বার</th>
-        <th>সাহরীর শেষ সময়</th>
-        <th>ইফতারের সময়</th>
-      </tr>
-    </thead>
-    <tbody>
-      {ramadanData?.map((single, index) => (
-        <>
-          {(index >= firstNumber && index <= lastNumber) && (
-            <tr>
-              <td>{replaceNumbers((index + 1).toString())}</td>
-              <td>{cascadeDate(single.date)}</td>
-              <td>{showDayInBangla(single.date)}</td>
-              <td>{'সকাল ' + replaceTimeString(single.sahri)}</td>
-              <td>{'সন্ধ্যা ' + replaceTimeString(single.iftar)}</td>
-            </tr>
-          )}
-        </>
-      ))}
-    </tbody>
-  </table>
-  )
-}
+      <thead>
+        <tr>
+          <th>রোজা</th>
+          <th>তারিখ</th>
+          <th>বার</th>
+          <th>সাহরীর শেষ সময়</th>
+          <th>ইফতারের সময়</th>
+        </tr>
+      </thead>
+      <tbody>
+        {ramadanData?.length ? (
+          ramadanData?.map((single, index) => (
+            <>
+              {index >= firstNumber && index <= lastNumber && (
+                <tr>
+                  <td>{replaceNumbers((index + 1).toString())}</td>
+                  <td>{cascadeDate(single.date)}</td>
+                  <td>{showDayInBangla(single.date)}</td>
+                  <td>{"সকাল " + replaceTimeString(single.sahri)}</td>
+                  <td>{"সন্ধ্যা " + replaceTimeString(single.iftar)}</td>
+                </tr>
+              )}
+            </>
+          ))
+        ) : (
+          <tr>
+            {Array(5)
+              ?.fill()
+              ?.map((single) => (
+                <>
+                  <td>
+                    {Array(3)
+                      ?.fill()
+                      ?.map((single) => (
+                        <Skeleton
+                          baseColor="#d4d6de"
+                          highlightColor="#a9adbd"
+                          duration={2}
+                        />
+                      ))}
+                  </td>
+                </>
+              ))}
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
 
-export default ReusableTable
+export default ReusableTable;
